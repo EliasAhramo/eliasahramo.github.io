a95ded23fff4920d95c8227de4f8afea21946346
Ahramo Elias Aaron <elias.ahramo@edu.tampere.fi>
2022-02-04 13:12:26 +0200

    fanikauppa

---
+++ fanikauppa/precache-manifest.a375908384f7ac844cd7c00f76240acf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d387c034318e82f1c8882212a5b7c8",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "04a5d302a04a0c7f8ce0",
+    "url": "/fanikauppa/static/css/main.cb487715.chunk.css"
+  },
+  {
+    "revision": "e81f27f3a9ef2d51622f",
+    "url": "/fanikauppa/static/js/2.4480cf88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.4480cf88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a5d302a04a0c7f8ce0",
+    "url": "/fanikauppa/static/js/main.07eb2737.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/fanikauppa/static/media/logo.ee7cd8ed.svg"
+  }
+]);
